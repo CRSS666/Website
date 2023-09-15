1fb549af61bd6cdaffd0d5f0f463e798a83a8448
TheClashFruit <clash.fruit123@gmail.com>
2023-09-15 16:47:56 +0200

    feat: routing

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
   require_once '_config.php';
-
-  echo $twig->render('index.twig', [
-    'user' => null
-  ]);
+  
+  switch ($_SERVER['REQUEST_URI']) {
+    case "/":
+      echo $twig->render('index.twig', [
+        'user' => null
+      ]);
+      break;
+    case "/nations":
+      echo $twig->render('nations.twig', [
+        'user' => null
+      ]);
+      break;
+    default:
+      http_response_code(404);
+      echo $twig->render('404.twig', [
+        'user' => null
+      ]);
+      break;
+  }
