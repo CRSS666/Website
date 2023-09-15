5758aee730194dbb08f5e72dd72bff5646923376
TheClashFruit <clash.fruit123@gmail.com>
2023-09-15 17:28:11 +0200

    feat: get logged in user in index.php

--- index.php
+++ index.php
@@ -5,21 +5,18 @@
 
   session_start();
   
+  if(isset($_SESSION['user']))
+    $twig->addGlobal('user', $_SESSION['user']);
+  
   switch ($_SERVER['REQUEST_URI']) {
     case "/":
-      echo $twig->render('index.twig', [
-        'user' => null
-      ]);
+      echo $twig->render('index.twig');
       break;
     case "/nations":
-      echo $twig->render('nations.twig', [
-        'user' => null
-      ]);
+      echo $twig->render('nations.twig');
       break;
     default:
       http_response_code(404);
-      echo $twig->render('404.twig', [
-        'user' => null
-      ]);
+      echo $twig->render('404.twig');
       break;
   }
