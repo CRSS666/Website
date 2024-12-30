60a81f8b08c411fc4fb609a8a3638a122550d3a6
blryface <me@blurry.gay>
2024-12-30 18:45:26 -0300

    feat: new description is this how i use commits

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,21 +17,13 @@ export default function Home() {
         <h1>Home</h1>
 
         <p>
-          Welcome to Clyde&apos;s Real Survival SMP, CRSS for short. We are a small SMP server that updates to every version starting from b1.0 on the 1st of every month. We have a small community of players that are very friendly and welcoming to new players. We have a few rules that you should follow to make the server a better place for everyone, you can find them at the bottom of the page.
+          Welcome to Clyde&apos;s Real Survival SMP, CRSS for short. We are a small SMP server that updates to every version starting from b1.0 on the 1st of every month. We have a small community of players that are mostly very friendly and welcoming to new players. We have a few rules that you should follow to make the server a better place for everyone, you can find them at the rules tab.
         </p>
-
         <p>
-          The server is built on the idea of nations, featuring { nations.length } nations so far, with the oldest being Panorama Socialist Federation, which was originally known as Republic of Panorama. These nations are scattered around the map, with some being more active than others. You can be sure to find a nation that fits your playstyle, if not you can just start your own!
+          As of 30/12/2024, the server has reached the latest version of Minecraft, which kind of kills its twist. The server will remain open until Clash or Oracle decide to nuke it, and there is a secondary server called CRSS2, which will stop updating at b1.7.3 if you're into it. CRSS2 info will be added to the website sometime in the future.
         </p>
-
-        <h2 id="modpacl">Modpack</h2>
-
         <p>
-          We have a client-side modpack to make your experince on CRSS better, the mods included are intended to be small, simple, and follow the rules of CRSS.
-        </p>
-
-        <p>
-          You can download it from Modrinth: <a href="https://modrinth.com/modpack/crsspack">https://modrinth.com/modpack/crsspack</a>.
+          The server is built on the idea of nations, featuring { nations.length } nations so far, with the oldest being the Republic of Panorama. These nations are scattered around the map, with some being more active than others. You can be sure to find a nation that fits your playstyle, if not you can just start your own!
         </p>
       </PageContent>
 
