16e005c35ab257e45b14c626580deae5cc968ab9
blryface <me@blurry.gay>
2025-03-19 17:08:24 -0300

    chore: remove ads, update description

--- components/AdBanner.tsx
+++ components/AdBanner.tsx
@@ -5,9 +5,12 @@ import { Info } from 'lucide-react';
 
 export default function AdBanner() {
   return (
+    /*
     <div className={styles.container} style={{ display: 'flex', justifyContent: 'center', marginBottom: '1.5rem' }}>
       <iframe id='a4962f15' name='a4962f15' src='https://ads.theclashfruit.me/www/delivery/afr.php?zoneid=3&amp;cb=75234' frameBorder='0' scrolling='no' width='468' height='60' allow='autoplay'></iframe>
     </div>
+    */
+
   );
 
   /*
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,10 +20,10 @@ export default function Home() {
           Welcome to Clyde&apos;s Real Survival SMP, CRSS for short. We are a small SMP server that updates to every version starting from b1.0 on the 1st of every month. We have a small community of players that are mostly very friendly and welcoming to new players. We have a few rules that you should follow to make the server a better place for everyone, you can find them at the rules tab.
         </p>
         <p>
-          As of 30/12/2024, the server has reached the latest version of Minecraft, which kind of kills its twist. The server will remain open until Clash or Oracle decide to nuke it, and there is a secondary server called CRSS2, which will stop updating at b1.7.3 if you&apos;re into it. CRSS2 info will be added to the website sometime in the future.
+        As of 30/12/2024, the server has reached the latest version of Minecraft, which kind of kills its twist. The server will remain open until Clash or Oracle decide to nuke it, and there is a third server called CRSS 3, which runs on b1.7.3 if you're into it. CRSS 3 info will be added to the website sometime in the future.
         </p>
         <p>
-          The server is built on the idea of nations, featuring { nations.length } nations so far, with the oldest being the Republic of Panorama. These nations are scattered around the map, with some being more active than others. You can be sure to find a nation that fits your playstyle, if not you can just start your own!
+          The server is built on the idea of nations, featuring 5 nations so far, with the oldest being the Republic of Panorama. These nations are scattered around the map, with some being more active than others. You can be sure to find a nation that fits your playstyle, if not you can just start your own!
         </p>
       </PageContent>
 
